f7ee1342668ff5cb60c51472ed5cf488904b8212
chaojun <chaojun@sina.com>
2019-06-09 16:49:30 +0800

    添加ant-design-pro-vue代码

--- user-auth/src/main/java/cn/t09/auth/sys/controller/UserController.java
+++ user-auth/src/main/java/cn/t09/auth/sys/controller/UserController.java
@@ -4,13 +4,16 @@ package cn.t09.auth.sys.controller;
 import cn.t09.auth.sys.entity.User;
 import cn.t09.auth.sys.service.IUserService;
 import cn.t09.common.AjaxResult;
-import javax.annotation.Resource;
-import org.aspectj.weaver.loadtime.Aj;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.aspectj.weaver.loadtime.Aj;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -21,16 +24,59 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2019-06-06
  */
 @RestController
-@RequestMapping("/sys/user")
+@RequestMapping("/sys/users")
 public class UserController {
 
     @Resource
     private IUserService userService;
 
-    @GetMapping("getById")
-    public AjaxResult getById(@RequestParam Long userId){
+    // 根据id获取用户信息
+    @GetMapping("{userId}")
+    public AjaxResult getById(@PathVariable Long userId) {
         User user = userService.getById(userId);
-        return AjaxResult.ok().put(AjaxResult.DATA_TAG,user);
+        return AjaxResult.ok().put(AjaxResult.DATA_TAG, user);
+    }
+
+    // 添加用户信息
+    @PostMapping
+    public AjaxResult add(@RequestBody User user) {
+        userService.save(user);
+        return AjaxResult.ok();
+    }
+
+    // 修改用户信息
+    @PutMapping
+    public AjaxResult update(@RequestBody User user) {
+        userService.updateById(user);
+        return AjaxResult.ok();
+    }
+
+    // 删除用户信息
+    @DeleteMapping
+    public AjaxResult delete(@RequestBody List<Long> userIds) {
+        // 系统管理员不能被删除
+        if (userIds.indexOf(1L) > 0) {
+            return AjaxResult.error("系统管理员不能被删除");
+        }
+
+        // 当前用户不能被删除
+
+        // 删除指定用户信息
+        userService.removeByIds(userIds);
+
+        return AjaxResult.ok();
+    }
+
+    // 分页获取用户信息
+    @GetMapping
+    public AjaxResult list(@RequestParam(value="p",defaultValue = "1")Long pageIndex,
+                           @RequestParam(value="s",defaultValue = "5")Long pageSize,
+                           User user) {
+        IPage<User> page  = new Page<>(pageIndex,pageSize);
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>(user);
+        queryWrapper.orderByDesc("create_time");
+        userService.page(page, queryWrapper);
+        return AjaxResult.ok().put("data",page);
     }
 
 }
--- user-auth/src/test/java/cn/t09/auth/sys/controller/UserControllerTest.java
+++ user-auth/src/test/java/cn/t09/auth/sys/controller/UserControllerTest.java
@@ -1,10 +1,16 @@
 package cn.t09.auth.sys.controller;
 
+import cn.t09.auth.sys.entity.User;
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * Created by gcjun on 2019/6/6.
@@ -15,10 +21,73 @@ public class UserControllerTest extends BaseTest {
     @Test
     public void getById() throws Exception {
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
-            .get("/sys/user/getById?userId=1");
-        ResultActions resultActions = mockMvc.perform(requestBuilder);
+                .get("/sys/users/1");
 
-        resultActions.andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(requestBuilder)
+        .andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    public void add() throws Exception {
+        User user = new User();
+        user.setUsername("lisi");
+        user.setPassword("123456");
+        user.setEmail("lisi@qq.com");
+        user.setMobile("12345678900");
+        user.setStatus(0);
+        user.setDeptId(1L);
+        user.setCreateTime(LocalDateTime.now());
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/sys/users").contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(JSON.toJSONString(user));
+
+        mockMvc.perform(requestBuilder)
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    public void update() throws Exception {
+        User user = new User();
+        user.setUserId(2L);
+        user.setUsername("lisi333");
+        user.setPassword("123456");
+        user.setEmail("lisi@qq.com");
+        user.setMobile("12345678900");
+        user.setStatus(0);
+        user.setDeptId(1L);
+        user.setCreateTime(LocalDateTime.now());
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/sys/users").contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(JSON.toJSONString(user));
+
+        mockMvc.perform(requestBuilder)
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void delete() throws Exception {
+        long[] ids = {2};
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/sys/users").contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(JSON.toJSONString(ids));
+
+        mockMvc.perform(requestBuilder)
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    public void list() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/sys/users");
+
+        mockMvc.perform(requestBuilder)
+                .andDo(MockMvcResultHandlers.print());
 
     }
 
